a0a2568cae3ee8b42397d10ae80334b9e0f1a4e6
nouret <rfnourettuz@gmail.com>
2015-03-31 10:14:22 +0300

    small rotations

--- main.cpp
+++ main.cpp
@@ -66,17 +66,104 @@ struct Elem{
         if (real()){
             left -> parent = this;
             right -> parent = this;
+            sum = 1 + left -> sum + right -> sum;
         }
+        else
+            sum = 0;
+    }
+    Elem * grandparent(){
+        if (this == NULL)
+            return NULL;
+        if (parent == NULL)
+            return NULL;
+        return parent -> parent;
+    }
+    Elem * uncle(){
+        Elem * G = grandparent();
+        if (G == NULL)
+            return NULL;
+        if (G -> left == parent)
+            return G -> right;
+        return G -> left;
+    }
+    void small_left_rotation(){
+        if (!this -> real)
+            return;
+        if (!right -> real)
+            return;
+        if (parent -> left == this){
+            parent -> left = right;
+        }
+        else{
+            parent -> right = right;
+        }
+        Elem * H = right;
+        right = H -> left;
+        H -> left = this;
+        H -> parent = parent;
+        recount_sum();
+        H -> recount_sum();
+        H -> parent -> recount_sum();
+    }
+    void small_right_rotation(){
+        if (!this -> real)
+            return;
+        if (!right -> real)
+            return;
+        if (parent -> left == this){
+            parent -> left = left;
+        }
+        else{
+            parent -> right = left;
+        }
+        Elem * H = left;
+        left = H -> right;
+        H -> right = this;
+        H -> parent = parent;
+        recount_sum();
+        H -> recount_sum();
+        H -> parent -> recount_sum();
+    }
+    void push_update(){
+        if (parent == NULL){
+            color = BLACK;
+            return;
+        }
+        if (parent -> color == BLACK){
+            return;
+        }
+        Elem * U = uncle();
+        Elem * G = grandparent();
+        if (U -> color == RED){
+            U -> color = BLACK;
+            parent -> color = BLACK;
+            G -> color = RED;
+            return;
+        }
+        //U -> color = BLACK;
+        //G -> color = BLACK;
+        if (G -> left == P && P -> right == N){
+            P -> small_left_rotation();
+            N = P;
+            P = N -> parent();
+        }
+        if (G -> right == P && P -> left == N){
+            P -> small_right_rotation();
+            N = P;
+            P = N -> parent();
+        }
+        
     }
 };
 
 struct Tree{
     Elem * root;
     Tree(){
-        root = Elem();
+        root = new Elem();
     }
     void push(double key){
         Elem * now = root;
+        Elem * h;
         while (now -> real()){
             if (key == now -> val)
                 return;
@@ -87,6 +174,13 @@ struct Tree{
         now -> val = key;
         now -> left = new Elem();
         now -> right = new Elem();
-        now -> recount_sun();
+        now -> recount_sum();
+        now -> color = RED;
+        h = now;
+        while (h -> parent != NULL){
+            h -> recount_sum();
+        }
+        h -> recount_sum();
+        now -> push_update();
     }
 };
